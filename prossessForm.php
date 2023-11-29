672115fbe032945f3d5af7d0652e433a61354ccf
unknown <B00140092@mytudublin.ie>
2023-11-29 15:54:59 +0000

    prossessForm.php added code that can insert data posted in the html into the database

--- prossessForm.php
+++ prossessForm.php
@@ -5,10 +5,16 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstname = $_POST['firstname'];
     $surname = $_POST['surname'];
+    $carReg = $_POST['carReg'];
+    $carMiles = $_POST['carMiles'];
+    $carYear = $_POST['carYear'];
     $nct = isset($_POST['nct']) ? 1 : 0;
 
     $_SESSION['clientFN'] = $firstname;
     $_SESSION['clientSN'] = $surname;
+    $_SESSION['clientCR'] = $carReg;
+    $_SESSION['clientCM'] = $carMiles;
+    $_SESSION['clientCY'] = $carYear;
     $_SESSION['nct'] = $nct;
 
     if ($nct == 1) {
