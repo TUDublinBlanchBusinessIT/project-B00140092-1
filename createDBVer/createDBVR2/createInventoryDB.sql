e283b23d9ce3fc12b929f9e1a4a6fd60bb786bac
unknown <B00140092@mytudublin.ie>
2023-12-08 13:02:30 +0000

    Added the separate versions of the SQL code that I have used including the one that I am currently using.

---
+++ createDBVer/createDBVR2/createInventoryDB.sql
@@ -0,0 +1,22 @@
+drop database if exists Inventory;
+create database Inventory;
+use Inventory;
+create table usedCars (
+    id int auto_increment,
+    carMake varchar(30),
+    carModel varchar(30),
+    carReg varchar(30), 
+    carMiles INT,
+    carYear INT,
+    nct INT DEFAULT 0,
+    primary key(id)
+);
+
+create table client (
+    id int auto_increment,
+    firstname varchar(30),
+    surname varchar(30),
+    phoneNumber INT,
+    primary key(id),
+    unique key(id)
+); 
